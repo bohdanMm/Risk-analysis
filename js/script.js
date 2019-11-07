e531435700e759c917b686b127bcba97a4f5786a
Bohdan <iabodia88@gmail.com>
2019-11-07 10:40:31 +0200

    Complete first stage

---
+++ js/script.js
@@ -0,0 +1,65 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    
+    checkSourceOfRisksTable();
+    checkIdentificationRiskTable();
+
+    document.getElementById('sourceOfRisksTable').addEventListener('change', checkSourceOfRisksTable);
+    document.getElementById('identificationRiskTable').addEventListener('change', checkIdentificationRiskTable);
+
+    function checkSourceOfRisksTable() {
+        var identificationRiskTable = document.getElementById('sourceOfRisksTable');
+        var counter = 0;
+        var allRows = 0;
+        var allUsedRows = 0;
+        for (var i = identificationRiskTable.getElementsByTagName('tr').length - 2; i > -1; i--) {
+            if (!identificationRiskTable.rows[i].classList.contains('main')) {
+                allRows++;
+            }
+        }
+        for (var i = identificationRiskTable.getElementsByTagName('tr').length - 2; i > -1; i--) {
+            if (identificationRiskTable.rows[i].classList.contains('main')) {
+                identificationRiskTable.rows[i].cells[1].innerHTML = counter;
+                identificationRiskTable.rows[i].cells[2].innerHTML = (counter/allRows*100).toPrecision(4) + '%';
+                counter = 0;
+            } else {
+                if (identificationRiskTable.rows[i].querySelector('.needWork').value == '1'){
+                    counter++;
+                    allUsedRows++;
+                }
+            }
+        }
+        identificationRiskTable.rows[identificationRiskTable.getElementsByTagName('tr').length - 1].cells[1].innerHTML = counter = allRows;
+        identificationRiskTable.rows[identificationRiskTable.getElementsByTagName('tr').length - 1].cells[2].innerHTML = counter = (allUsedRows/allRows*100).toPrecision(4)+'%';
+    }
+
+    function checkIdentificationRiskTable() {
+        var identificationRiskTable = document.getElementById('identificationRiskTable');
+        var counter = 0;
+        var allRows = 0;
+        var allUsedRows = 0;
+        for (var i = identificationRiskTable.getElementsByTagName('tr').length - 2; i > -1; i--) {
+            if (!identificationRiskTable.rows[i].classList.contains('main')) {
+                allRows++;
+            }
+        }
+        for (var i = identificationRiskTable.getElementsByTagName('tr').length - 2; i > -1; i--) {
+            if (identificationRiskTable.rows[i].classList.contains('main')) {
+                identificationRiskTable.rows[i].cells[1].innerHTML = counter;
+                identificationRiskTable.rows[i].cells[2].innerHTML = (counter/allRows*100).toPrecision(4) + '%';
+                counter = 0;
+            } else {
+                if (identificationRiskTable.rows[i].querySelector('.needWork').value == '1'){
+                    counter++;
+                    allUsedRows++;
+                }
+            }
+        }
+        identificationRiskTable.rows[identificationRiskTable.getElementsByTagName('tr').length - 1].cells[1].innerHTML = counter = allRows;
+        identificationRiskTable.rows[identificationRiskTable.getElementsByTagName('tr').length - 1].cells[2].innerHTML = counter = (allUsedRows/allRows*100).toPrecision(4)+'%';
+    }
+
+    function selectChecker(){
+        Element
+    }
+
+});
